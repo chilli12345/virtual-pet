535877d8c29c104720b9678aacb4029a7cf6e0c3
chilli12345 <73940796+chilli12345@users.noreply.github.com>
2021-01-24 18:47:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,20 +2,28 @@
 var dog,happyDog , database, foodS, foodStock;
 var dogImage;
 var database;
+var hall;
+var bowl;
+var spriteBowl;
 
 function preload()
 {
   //load images here
   dogImage = loadImage("images/dogImg.png");
   happyDog = loadImage("images/dogImg1.png");
-
+  hall = loadImage ("images/hall.jpg");
+  bowl = loadImage("images/bowl.png");
 }
 
 function setup() {
 	createCanvas(500, 500);
-  dog=createSprite(250,380,20,20);
+  dog=createSprite(320,420,20,20);
   dog.addImage(dogImage);
-  dog.scale=0.1;
+  dog.scale=0.15;
+
+  spriteBowl=createSprite(210,460,10,10);
+  spriteBowl.addImage(bowl);
+  spriteBowl.scale=0.15;
   database=firebase.database();
   foodStock=database.ref("Food");
   foodStock.on("value",readStock);
@@ -23,14 +31,15 @@ function setup() {
 
 
 function draw() {  
-  background(46,139,87);
+  background(hall);
   drawSprites();
   //add styles here
-  fill("white");
+  fill("black");
   textSize(15);
-  text("NOTE: Press UP_ARROW to feed drago milk",120,40);
+  text("NOTE: Press UP_ARROW to feed drago milk",120,60);
+  fill("white");
   textSize(20);
-  text("Food Remaining: "+foodS,170,280);
+  text("Food Remaining: "+foodS,180,350);
  if(keyWentDown(UP_ARROW)){
    writeStock(foodS);
    dog.addImage(happyDog);
